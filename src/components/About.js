4edc6e884e3089cc7e6d1531eb9f468400d3bb70
unknown <harikadanduprolu740@gmail.com>
2024-11-04 17:09:21 +0530

    fourth commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 // src/components/About.js
 import React from 'react';
-import './About.css'; // Ensure this CSS file is styled for the new layout
+import './About.css';
 
 const About = () => {
   const teamMembers = [
@@ -40,20 +40,27 @@ const About = () => {
     <section className="about-section">
       <div className="about-content">
         <h2>About Us</h2>
-        <p>We are dedicated to providing top legal services with a specialized team of advocates.</p>
-        <div className="team">
-          {teamMembers.map((member, index) => (
-            <div className="team-member" key={index}>
-              <div className="member-image">
-                <img src={member.image} alt={member.name} />
-              </div>
-              <div className="member-info">
-                <h3>{member.name}</h3>
-                <p>{member.specialization}</p>
-              </div>
+        <p>
+          Welcome to <strong>lawmine.in</strong>. Since 2014, our team of advocates has been providing legal services across India. 
+          With a principal office in Hyderabad and branches in Delhi, Mumbai, Bangalore, Chennai, and Kolkata, we support our clients in various legal matters. 
+          Our advocates are skilled in drafting, filing, and arguing cases in trial courts, tribunals, competent authorities, commissions, High Courts, and the Supreme Court. 
+          We keep our clients informed on case updates and address any questions on case proceedings. Our team has successfully handled civil, criminal, corporate cases, 
+          public interest cases, and some cases have been landmark judgments for public welfare.
+        </p>
+      </div>
+      <div className="team">
+      <h2>Our Advocates</h2>
+        {teamMembers.map((member, index) => (
+          <div className="team-member" key={index}>
+            <div className="member-image">
+              <img src={member.image} alt={member.name} />
             </div>
-          ))}
-        </div>
+            <div className="member-info">
+              <h3>{member.name}</h3>
+              <p>{member.specialization}</p>
+            </div>
+          </div>
+        ))}
       </div>
       <div className="about-image">
         <img src="path_to_your_image.jpg" alt="Legal team" />
